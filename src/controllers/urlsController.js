6bf5097c1296e7aa39288b4c65f96d1b68649d58
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-03 11:22:19 -0300

    refactor: send correct status when new url is created

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -5,10 +5,16 @@ export async function postNewUrl(req, res) {
     const { url } = req.body;
     const shortenedUrl = nanoid();
     console.log(shortenedUrl);
+    
+    try {
+        
+        const result = await connection.query(
+            `INSERT INTO links (user_id, url, short_url, visitors) VALUES ($1, $2, $3, $4);`,
+            [res.locals.user.id, url, shortenedUrl, 0]);
 
-    const result = await connection.query(
-        `INSERT INTO links (user_id, url, short_url, visitors) VALUES ($1, $2, $3, $4);`,
-        [res.locals.user.id, url, shortenedUrl, 0]);
-
-    res.send(result);
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(chalk.white.bgRed("ERRO URLS:") + error);
+        res.sendStatus(500);
+    }
 }
